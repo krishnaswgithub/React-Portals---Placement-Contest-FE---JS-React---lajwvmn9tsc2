76f3f2d6fc5aecc1576de4d366395438f2fd654e
Krishna Das <120264734+krishnaswgithub@users.noreply.github.com>
2023-09-24 19:04:37 +0530

    Update PortalTextArea.js

--- src/components/PortalTextArea.js
+++ src/components/PortalTextArea.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import  ReactDOM  from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
 //complete this function using portals
-const PortalTextArea=()=>{
-    return (
-        <div>
-            <textarea id="textarea" value={}></textarea>
-        </div>
-    )
-}
+const PortalTextArea = ({ valu }) => {
+  return ReactDOM.createPortal(
+    <div>
+      <textarea id="textarea" value={valu}></textarea>
+    </div>,
+    document.getElementById("portal-textarea"),
+  );
+};
 export default PortalTextArea;
 //portal-textarea id will be used here for portal purpose
